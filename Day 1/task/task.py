8ebb2780ec65c93612c5b0d3bee011b2eaa97677
Chika Nkemjika <chikankemjika2024@gmail.com>
2026-08-07 13:01:49 +0100

    Day 1: Band Name Generator complete

---
+++ Day 1/task/task.py
@@ -0,0 +1,6 @@
+print("Welcome to Chika's Band Name Generator")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+
+band_name = city + " " + pet
+print("Your Band name Could be: " + band_name)
